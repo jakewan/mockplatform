826ad99f515c574f637a5e7f5bb06052fb93e09a
Jacob Wan <jacobwan840@gmail.com>
2020-09-21 16:38:57 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup_kwargs = {
     'description': 'Sometimes things go wrong',
     'long_description': get_long_description(),
     'long_description_content_type': 'text/markdown',
-    'version': '0.1.1',
+    'version': '0.2.0',
     'url': 'https://github.com/jakewan/mockplatform',
     'author': 'Jacob Wan',
     'author_email': 'jacobwan840@gmail.com',
